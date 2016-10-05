edf1196c0b47a2edf7e917c3f69231d1cb18ee3a
smbss <sandro.simes92@gmail.com>
2016-10-05 15:56:59 +0100

    Installed and configured Firebase for push notifications

--- PushingNotifs/AppDelegate.swift
+++ PushingNotifs/AppDelegate.swift
@@ -2,11 +2,12 @@
 //  AppDelegate.swift
 //  PushingNotifs
 //
-//  Created by Sandro Simes on 05/10/2016.
+//  Created by smbss on 05/10/2016.
 //  Copyright © 2016 smbss. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
         return true
     }
 
